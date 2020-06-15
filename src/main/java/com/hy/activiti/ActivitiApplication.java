2ec2bafe5ee43e5e7ba69f806b42d257bc06fc3e
pattyforu811 <49299860+pattyforu811@users.noreply.github.com>
2020-06-15 19:51:20 +0800

    hy

--- src/main/java/com/hy/activiti/ActivitiApplication.java
+++ src/main/java/com/hy/activiti/ActivitiApplication.java
@@ -1,16 +1,17 @@
 package com.hy.activiti;
 
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"com.hy"})
-@MapperScan(basePackages = {"com.hy"})
+//@ComponentScan(basePackages = {"com.hy"})
+//@MapperScan(basePackages = {"com.hy"})
 //@SpringBootApplication(exclude = ActivitiSpringIdentityAutoConfiguration.class)
-//@SpringBootApplication
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
+//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ActivitiApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/hy/activiti/ActivitiApplicationTests.java
+++ src/test/java/com/hy/activiti/ActivitiApplicationTests.java
@@ -1,7 +1,17 @@
 package com.hy.activiti;
 
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.DeploymentBuilder;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest
 class ActivitiApplicationTests {
@@ -9,5 +19,16 @@ class ActivitiApplicationTests {
     @Test
     void contextLoads() {
     }
+
+
+
+    @Autowired
+    private RepositoryService repositoryService;
+    @Autowired
+    private RuntimeService runtimeService;
+    @Autowired
+    private TaskService taskService;
+
+
 
 }
